9b216955e2817fd45f758d6b594adf9462263a90
Trang3598 <nguyenthikieutrang_t61@hus.edu.vn>
2020-06-18 20:41:43 +0700

    messenger

--- resources/views/client/master.blade.php
+++ resources/views/client/master.blade.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="{{asset('client/css/bootstrap.min.css')}}">
     {{--    <link rel="stylesheet" href="{{asset('client/css/home.css')}}">--}}
     <link rel="stylesheet" href="{{asset('client/css/search.css')}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <script type="text/javascript" src="{{asset('client/js/jquery-3.2.1.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
     <script type="text/javascript" src="{{asset('client/js/bootstrap.min.js')}}"></script>
@@ -41,12 +42,13 @@
         }
     </style>
 </head>
-<body >
+<body>
 <!-- header -->
-<header id="header" >
+<header id="header">
     <div class="col-sm-12">
-        <div class="row header-content" style="display: flex;flex-direction: row;justify-content: center;padding: 20px 0">
-            <div id="logo" class="col-md-3 col-sm-12 col-xs-12" >
+        <div class="row header-content"
+             style="display: flex;flex-direction: row;justify-content: center;padding: 20px 0">
+            <div id="logo" class="col-md-3 col-sm-12 col-xs-12">
                 <h1>
                     <a href="#"><img src="{{asset('client/img/home/shop.png')}}"></a>
                     <nav><a id="pull" class="btn btn-danger" href="#">
@@ -55,7 +57,8 @@
                 </h1>
             </div>
             <div class="col-md-7  header-middle" style="display: flex;flex-direction: column;flex-wrap: wrap">
-                <div class="list-items" style="height: 50%; display: flex;flex-direction: row;flex-wrap: wrap; align-items: center">
+                <div class="list-items"
+                     style="height: 50%; display: flex;flex-direction: row;flex-wrap: wrap; align-items: center">
                     <div class="item-1" style="margin-left: 5%;">
                         <i class="fas fa-truck-moving" style="font-size: 20px;"></i> <span class="c-1">GIAO HÀNG MIỄN PHÍ</span>
                     </div>
@@ -69,16 +72,20 @@
                 <div class="search-form" style="height: 50%">
                     <form action="{{route('search-product')}}" method="post" autocomplete="off">
                         @csrf
-                        <div class="form-group row" >
+                        <div class="form-group row">
                             <div class="col-sm-8" style="display: flex;flex-direction: row">
-                                <input type="text" class="form-control" id="search-data" value="" placeholder="Bạn tìm gì..." name="searchData">
-                                <button type="submit" class="btn col-sm-2" style="height: 45px;background-color: #00BCD4"><i class="fa fa-search " ></i></button>
+                                <input type="text" class="form-control" id="search-data" value=""
+                                       placeholder="Bạn tìm gì..." name="searchData">
+                                <button type="submit" class="btn col-sm-2"
+                                        style="height: 45px;background-color: #00BCD4"><i class="fa fa-search "></i>
+                                </button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-            <div id="cart" class="col-md-2 col-sm-12 col-xs-12" style="display: flex;align-items: center;flex-direction: row">
+            <div id="cart" class="col-md-2 col-sm-12 col-xs-12"
+                 style="display: flex;align-items: center;flex-direction: row">
                 <a class="display" href="#">
                     <i class="fas fa-shopping-cart" style="font-size: 50px; color: #00BCD4"></i></a>
                 <a href="{{route('order.item')}}" style="margin-left: -25px;margin-top: -37px;color: white">
@@ -93,9 +100,10 @@
 </header><!-- /header -->
 <section class="menu-bar">
     {{--    <div class="col-sm-12">--}}
-    <div class="menu-bar-content " style="display: flex;flex-direction: row;flex-wrap: wrap; background-color: #2990ea; padding: 0 10px;color: white;height: 60px">
+    <div class="menu-bar-content "
+         style="display: flex;flex-direction: row;flex-wrap: wrap; background-color: #2990ea; padding: 0 10px;color: white;height: 60px">
         <div class="m-1 list-products col-sm-3" style="margin: 0 !important;">
-            <div  style="background-color: #1f6fb2;padding: 19px 20px;">
+            <div style="background-color: #1f6fb2;padding: 19px 20px;">
                 <i class="fas fa-bars"></i> Danh mục sản phẩm
             </div>
         </div>
@@ -124,9 +132,9 @@
     <div class="col-sm-12">
         <div class="row">
             <div class="col-sm-12" style="display: flex;flex-direction: row">
-                <div id="sidebar"  class="col-sm-3">
+                <div id="sidebar" class="col-sm-3">
                     <nav id="menu">
-                        <ul >
+                        <ul>
                             @foreach($categories as $category)
                                 <li class="menu-item" id="product_{{$category->id}}"><a
                                         href="{{route('category.product',$category->id)}}" title=""
@@ -144,13 +152,16 @@
                                 <li data-target="#demo" data-slide-to="1"></li>
                                 <li data-target="#demo" data-slide-to="2"></li>
                             </ul>
-                            <div class="carousel-inner" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                                <div class="carousel-item active" >
-                                    <img src="{{asset('images/' .$slides[0]->image)}}" alt="Los Angeles" style="width: 100%">
+                            <div class="carousel-inner"
+                                 style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                                <div class="carousel-item active">
+                                    <img src="{{asset('images/' .$slides[0]->image)}}" alt="Los Angeles"
+                                         style="width: 100%">
                                 </div>
                                 @for($i = 0;$i<sizeof($slides);$i++)
                                     <div class="carousel-item">
-                                        <img src="{{asset('images/'.$slides[$i]->image)}}" alt="New York"  style="width: 100%">
+                                        <img src="{{asset('images/'.$slides[$i]->image)}}" alt="New York"
+                                             style="width: 100%">
                                     </div>
                                 @endfor
                             </div>
@@ -167,8 +178,9 @@
                 </div>
             </div>
             <div class="col-sm-12" style="margin-top: 10px; display: flex;flex-direction: row;flex-wrap: wrap">
-                <div class="col-sm-6 sale" >
-                    <img src="{{asset('client/img/home/banner-t-1.png')}}" alt="Los Angeles" style="width: 100%" class="">
+                <div class="col-sm-6 sale">
+                    <img src="{{asset('client/img/home/banner-t-1.png')}}" alt="Los Angeles" style="width: 100%"
+                         class="">
                 </div>
                 <div class="col-sm-6 sale">
                     <img src="{{asset('client/img/home/banner-t-2.png')}}" alt="Los Angeles" style="width: 100%">
@@ -189,12 +201,12 @@
             <div id="logo" class="col-sm-3 text-center">
                 <a href="#"><img src="{{asset('client/img/home/logo1.png')}}"></a>
             </div>
-            <div  class="col-sm-3">
+            <div class="col-sm-3">
                 <h3>Về chúng tôi</h3>
                 <p class="text-justify">Sky Shop thành lập năm 2020. Chúng tôi luôn đem đến chất lượng dịch vụ tốt
                     nhất và những chính sách ưu đãi cho quý khách</p>
             </div>
-            <div  class="col-sm-3">
+            <div class="col-sm-3">
                 <h3>Liên hệ</h3>
                 <p>Phone Sale: (+84) 0357 589 900</p>
                 <p>Email: trangntk98@gmail.com</p>
@@ -205,6 +217,33 @@
                 <p>Address 2: Thanh Phong - Thanh Liêm - Hà Nam</p>
             </div>
         </div>
+    </div>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function () {
+            FB.init({
+                xfbml: true,
+                version: 'v7.0'
+            });
+        };
+
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+         attribution=setup_tool
+         page_id="105324574561145"
+         theme_color="#0084ff"
+         logged_in_greeting="Sky Shop xin chào quý khách !"
+         logged_out_greeting="Sky Shop xin chào quý khách !">
     </div>
 </footer>
 <!-- endfooter -->
